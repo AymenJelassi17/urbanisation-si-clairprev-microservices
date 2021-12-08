5f718081767f6a2070d41b2c73a2b554a0bc6e7d
Aymen <aymen17@hotmail.fr>
2021-12-08 01:43:08 +0100

    TP1 microservices journée autonomie

---
+++ microservices_assure/src/main/java/com/urbanisationsi/microservices_assure/modele/Personne.java
@@ -0,0 +1,57 @@
+package com.urbanisationsi.microservices_assure.modele;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name="Personne_type")
+public abstract class Personne {
+	@Id    
+    @GeneratedValue(strategy=GenerationType.AUTO)     
+    private Integer id;
+
+    private String nom;
+
+    private String prenom;
+
+    private Long numeroPersonne;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Long getNumeroPersonne() {
+		return numeroPersonne;
+	}
+
+	public void setNumeroPersonne(Long numeroPersonne) {
+		this.numeroPersonne = numeroPersonne;
+	}
+
+}
